4ce6fcd6ebf90b9df784f3d78b45f54021e66fab
Sebastian Harteneck <sharteneck@gmail.com>
2015-02-12 20:01:26 -0300

    1er commit

---
+++ javascript2.js
@@ -0,0 +1,43 @@
+// colleccion 
+// edad = key
+// 40 = value
+
+var objecto = {edad : 40, nacionalidad: "argentino"};
+
+var 2 = persona.edad 
+
+var 1 = persona.["edad"] 
+
+var personaDos = {
+	cantOjo : 2,
+	nombre: "Pepe",
+	cantidadAutos: [],
+	cantidadHijos: 2;
+}
+
+
+-------
+
+var auto1 = {
+	año: 2007;
+	dni: 28642282;
+	marca: "Ferrari";
+}
+
+var auto2 = {
+	año: 2013;
+	dni: 30282649;
+	marca: "Mercedez Benz";
+}
+
+var auto3 = {
+	año: 2014;
+	dni: 30282649;
+	marca: "Mc Laren P1";
+}
+
+var auto4 = {
+	año: 2015;
+	dni: 30282649;
+	marca: "Bmw M5";
+}
