d29f2b6efd8be6a82347f08780006afc2a74221d
Sebastian Harteneck <sharteneck@gmail.com>
2015-02-12 20:20:01 -0300

    3do commit

--- javascript2.js
+++ javascript2.js
@@ -1,28 +1,31 @@
-
--------
+//Clase Objetos
 
 var auto1 = {
-	año: 2007;
-	dni: 28642282;
-	marca: "Ferrari";
+	año: 2007,
+	dni: 28642282,
+	marca: "Ferrari",
 }
 
 var auto2 = {
-	año: 2013;
-	dni: 30282649;
-	marca: "Mercedez Benz";
+	año: 2013,
+	dni: 30282649,
+	marca: "Mercedez Benz",
 }
 
 var auto3 = {
-	año: 2014;
-	dni: 30282649;
-	marca: "Mc Laren P1";
+	año: 2014,
+	dni: 30282649,
+	marca: "Mc Laren P1",
 }
 
 var auto4 = {
-	año: 2015;
-	dni: 30282649;
-	marca: "Bmw M5";
+	año: 2015,
+	dni: 30282649,
+	marca: "Bmw M5",
 }
 
- 
+
+auto1.velocidadMaxima = "320";
+auto2.velocidadMaxima = "280";
+auto3.velocidadMaxima = "370";
+auto4.velocidadMaxima = "290";
